4bcc7736c923d94c0c4e4b17a9b253f46a5e2848
Srihari Venkatesan <sri.venkatesan23@gmail.com>
2016-08-31 22:34:22 -0400

    Youtube playlist maker

--- src/redditBot.py
+++ src/redditBot.py
@@ -1,24 +1,109 @@
 import praw
 import youtube_dl
+import httplib2
+import os
+import sys
+from urllib.parse import urlparse
+
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser
+from oauth2client.tools import run_flow
+
+
 
 
 r = praw.Reddit(user_agent='LTTscraper')
 submissions = r.get_subreddit('listentothis').get_top_from_week(limit=10)
+CLIENT_SECRETS_FILE = "keys.json"
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
 
-save_path = "C:\\Users\\Sri\\Desktop\\listentothis.txt"
-dl_path = "C:\\Users\\Sri\\Desktop\\listentothisdls"
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+   %s
+with information from the Developers Console
+https://console.developers.google.com/
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '256',
-    }],
-}
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+  message=MISSING_CLIENT_SECRETS_MESSAGE,
+  scope=YOUTUBE_READ_WRITE_SCOPE)
+
+storage = Storage("%s-oauth2.json" % sys.argv[0])
+credentials = storage.get()
+
+if credentials is None or credentials.invalid:
+  flags = argparser.parse_args()
+  credentials = run_flow(flow, storage, flags)
+
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+  http=credentials.authorize(httplib2.Http()))
+
+playlists_insert_response = youtube.playlists().insert(
+  part="snippet,status",
+  body=dict(
+    snippet=dict(
+      title="ListenToThis",
+      description="A private playlist containing the week's top songs from r/ListenToThis"
+    ),
+    status=dict(
+      privacyStatus="private"
+    )
+  )
+).execute()
+
+def add_video(youtube, videoID, playlistID):
+    add_video_request = youtube.playlistItem().insert(
+        part="snippet",
+        body={
+            'snippet': {
+                'playlistId': playlistID,
+                'resourceId': {
+                    'kind': 'youtube#video',
+                    'videoId': videoID
+                }
+                # 'position': 0
+            }
+        }
+    ).execute()
+
+
+
+def video_id(link):
+
+    query = urlparse(link)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = urlparse.parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    return None
+
+
+
+
 for s in submissions:
-
     if "youtube" in s.url:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:  #import downloader and save as ydl variable
-            ydl.download([s.url]) #download audio from URL of each submission
+        add_video(youtube, video_id(s.url), playlists_insert_response["id"])
+
+
+
+
+
+
+
 
